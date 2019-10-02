73f794f0e3767581b9723e74396a7bfc5f1ddfee
GustavoM7 <gustavosmarquesf@gmail.com>
2019-10-02 15:27:58 -0300

    implementação

--- test/src/App.js
+++ test/src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import Dropzone from 'react-dropzone';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  state = {
+    file: null,
+  }
+
+  acceptFile = (file) =>{
+    console.log(file)
+    this.setState({file: file[0]});
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+
+        <Dropzone onDrop={acceptedFiles => this.acceptFile(acceptedFiles)}>
+          {({getRootProps, getInputProps}) => (
+            <section>
+              <div {...getRootProps()}>
+                <input {...getInputProps()} type="file" accept="image/x-png,image/gif,image/jpeg" />
+                {this.state.file !== null? (<p>{this.state.file.path}</p>) : null}
+                <p>Clique aqui ou arraste uma nova imagem</p>
+                
+              </div>
+            </section>
+          )}
+        </Dropzone>
+      </div>
+    );
+  }
 }
 
 export default App;
